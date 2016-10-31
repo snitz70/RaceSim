96b1d0aaea68a76b5cf56ee777d710a481bd1234
Brian Snyder <bsnyder@technology4retailers.com>
2016-10-31 12:16:51 -0500

    finished skeleton

---
+++ RaceSim/RaceSim/Bet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceSim
+{
+    public class Bet
+    {
+        public int Amount;
+        public int Dog;
+        public Guy Bettor;
+
+        public string GetDescription()
+        {
+            // Return a string that says who placed the bet, ow much
+            // cash was bet, and which dog he bet on ("Joe bets 8 on
+            // dog #4").  If the amount is zero, not bet was placed
+            // ("Joe hasn't placed a bet")
+        }
+
+        public int PayOut(int Winner)
+        {
+            // The parameter is the winner of the race.  If the dog won,
+            // return the amount of be.  Otherwise, return the negative of
+            // the amount bet.
+        }
+    }
+}
---
+++ RaceSim/RaceSim/Guy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RaceSim
+{
+    public class Guy
+    {
+        public string Name;
+        public Bet MyBet;
+        public int Cash;
+
+        // the last two fields are the guy's GUI controls on the form
+        public RadioButton MyRadioButton;
+        public Label MyLabel;     
+
+        public void UpdateLabels()
+        {
+            // Set my label to my bet's description and the lable on my
+            // radio button to show my cash ("Joe has 43 bucks")
+        }
+
+        public void ClearBet()
+        {
+            // Reset my bet so it's zero
+        }
+
+        public void PlaceBet(int BetAmount, int DogToWin)
+        {
+            // Place a new bet and store it in my bet field
+            // Return true if the guy had enough money to bet
+        }
+
+        public void Collect(int Winner)
+        {
+            // Ask my bet to pay out, clear my bet, and update my lables
+        }
+    }
+}
